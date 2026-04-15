68fc34a2cca3c2f06cbe9eb40f39fbbf91c50b36
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2026-04-16 00:36:36 +0530

    add triangular network

--- topology.py
+++ topology.py
@@ -44,7 +44,34 @@ class HierarchicalTopo(Topo):
         self.addLink(s3, h4, bw=100, delay="1ms")
 
 
+class TriangleTopo(Topo):
+    """
+    Triangle topology for topology change detection testing
+
+    Topology:
+        h1 --- s1 --- s2 --- h2
+                \     /
+                 s3
+    """
+
+    def build(self):
+        h1 = self.addHost("h1", mac="00:00:00:00:00:01", ip="10.0.0.1/24")
+        h2 = self.addHost("h2", mac="00:00:00:00:00:02", ip="10.0.0.2/24")
+
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+
+        self.addLink(s1, s2, bw=100, delay="1ms")
+        self.addLink(s1, s3, bw=100, delay="1ms")
+        self.addLink(s2, s3, bw=100, delay="1ms")
+
+
 topos = {
     "single": (lambda: SingleSwitchTopo()),
+    "triangle": (lambda: TriangleTopo()),
     "hierarchical": (lambda: HierarchicalTopo()),
 }
